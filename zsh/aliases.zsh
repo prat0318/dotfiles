b75001afe79c028d81cb4764ceda524404b17091
Prateek Agarwal <prat0318@gmail.com>
2015-01-15 00:07:00 +0530

    Add iterm2 config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,24 +1,8 @@
 alias reload!='exec zsh'   # alias reload!='. ~/.zshrc'
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
 alias version="lsb_release -a"
-
 alias vi="vim -p"
 
-alias pm="python manage.py"
-alias pycharm='cwd=$(pwd);cd ~/setups/pycharm-community-3.0/bin; echo | sh pycharm.sh &; cd $cwd; cwd='
-
-alias idea="/home/prat0318/setups/idea-IC-133.193/bin/idea.sh"
-
-alias projects="cd ~/projects"
-alias epl="cd ~/projects/epl"
-alias gitator="cd ~/projects/gitator"
-
-alias g++="g++ -std=c++11"
-alias epl="cd ~/projects/epl"
-alias clingo="~/setups/clingo-4.2.1-x86-linux/clingo"
-
 alias l="ls -lAhrt"
-alias ll="ls -lhrt"
-alias angry="ssh angry-goat.cs.utexas.edu"
+
+# alias pm="python manage.py"
+
